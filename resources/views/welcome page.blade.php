619ebd449d4c2f066ec211b26a3140a6fa83d033
Ikramullahqayyum <Ikramullahqayyumi2@gmail.com>
2022-05-23 01:41:31 +0700

    I already updated the welcome page

---
+++ resources/views/welcome page.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:weight@100;200;300;400;500;600;700;800&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/welcome.css" >
+    <title>Welcome Page</title>
+</head>
+<body>
+    <div class="container bg-welcome">
+        <div class="row text-center">
+            <h1 class="text-dark">ALL YOUR EVENTS  IN A POCKET</h1>
+        </div>
+    </div>
+    <div class="container bg-blue" >
+               
+    <div class="card">
+        <div class="row justify-content-between text-center">
+        <div class="col-4">
+        <img src="/gambar/Star.png" alt="">
+        <p>Check Out Highlights Event</p>
+        </div>
+        <div class="col-4">
+        <img src="/gambar/bookmark-line.png" alt="">
+        <p>Save Your Upcoming Event</p>
+        </div>
+        <div class="col-4">
+        <img src="/gambar/upload-line.png" alt="">
+        <p>Organizer? Register your Event</p>
+        </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
