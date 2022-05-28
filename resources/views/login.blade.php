e27dee2323551379ad7ea7e1ee3d5013cc713eaa
petersuryah1 <pitersuryah3@gmail.com>
2022-05-29 00:55:54 +0700

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" >
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css"
+      integrity="sha512-thoh2veB35ojlAhyYZC0eaztTAUhxLvSZlWrNtlV01njqs/UdY3421Jg7lX0Gq9SRdGVQeL8xeBp9x1IPyL1wQ=="
+      crossorigin="anonymous"
+    >
+    <title>Register</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-2">
+        <div class="buttonback">
+            <img src="/gambar/backbutton.png" alt="" style="margin-left: 19px; margin-top: 23px">
+        </div>
+    </div>
+    <div class="col-8">
+        <p></p>
+    </div>
+    <div class="col-2" style="margin-top:15px;">
+        <div class="menu">
+            <img src="/gambar/Menu.png" alt="">
+         </div>
+    </div>
+    </div>
+
+    <div class="pockitsimg">
+        <img src="/gambar/pockits gede.png" alt="" style="margin-left: 59px; margin-right: 60px; margin-top: 22px;">
+    </div>
+
+    <div class="word">
+        <p style="font-family: 'Open Sans'; font-size: 28px; margin-left: 32px;">
+            Login
+        </p>
+    </div>
+    <div class="formstudent" style="margin-left: 28px; margin-right: 37px">
+        <form action="/action_page.php">
+            <div class="mb-3 mt-3">
+              <label for="email" class="form-label">Email:</label>
+              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+            </div>
+            <div class="mb-3">
+              <label for="pwd" class="form-label">Password:</label>
+              <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+            </div>
+    </form>
+    </div>
+    <p style="bac"></p>
+    <div class="button1" style="margin-left: 32px; margin-right: 22px;">
+        <a href="#" class="btn btn-success" style="border-radius: 12px; background-color: #023880; width: 262px;
+        height: 52px; font-family: 'Open Sans'; font-size: 18px;">
+        <p style="margin-top: 5px;">Submit</p>
+    </a>
+    </div>
+</div>
+</body>
+</html>
